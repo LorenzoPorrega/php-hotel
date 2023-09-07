2331e0f15c688a5943e18250067468d93087313f
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-09-07 17:32:12 +0200

    create repo and foreach cycle

---
+++ filtered.php
@@ -0,0 +1,110 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+    //$availableParking = array_filter($hotels, ['parking',  false]);
+    //$unavailableParking = array_diff($hotels, ['parking', true]);
+    $parkingFilter = $_POST["parkingFilter"];
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trivago</title>
+  <!-- CUSTOM STYLE SHEET FILE LINK -->
+  <!--<link rel="stylesheet" href="./css/style.css">-->
+  
+  <!-- THIRD PARTY LIBRARIES -->
+  <!-- BOOTSTRAP CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <!-- FONTAWESOME CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- STANDARD FAVICON TO AVOID CONSOLE ERRORS ON FIREFOX -->
+  <link rel="icon"type="image/x-icon" href="https://boolean.careers/favicon/favicon.ico">
+  <!-- VUE CDN -->
+  <!--<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>-->
+  <!-- LUXON CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/luxon@3.3.0/build/global/luxon.min.js"></script>
+  <!-- AXIOS CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+<body>
+  <div class="container text-center mt-5">
+  <h2 class="fw-bold">Hai scelto di visualizzare gli hotel <?php echo $parkingFilter === "1" ? "<span p class='text-decoration-underline'>con parcheggio</span>" : "<span p class='text-decoration-underline'>senza parcheggio</span>" ?></h2>
+    <table class="table mt-5 table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Parcheggio</th>
+          <th scope="col">Voto</th>
+          <th scope="col">Distanza dal centro (km)</th>
+        </tr>
+      </thead>
+      <tbody>
+          <?php if($parkingFilter = true){
+            echo $parkingFilter . ' selezionato';
+          }
+          if($parkingFilter = 2){
+            foreach ($hotels as $singleHotel){
+          ?>
+            <tr>
+              <th scope="row">
+                <?php echo $singleHotel['name']?>
+              </th>
+              <td>
+                <?php echo $singleHotel['parking'] ? "Sì" : "No" ?>
+              </td>
+              <td>
+                <?php echo $singleHotel['vote']?>
+              </td>
+              <td>
+                <?php echo $singleHotel['distance_to_center']?> km
+              </td>
+            </tr>
+          <?php 
+            }
+          }
+          ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+<!--<script src="./js/main.js"></script>-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,130 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trivago</title>
+  <!-- CUSTOM STYLE SHEET FILE LINK -->
+  <!--<link rel="stylesheet" href="./css/style.css">-->
+  
+  <!-- THIRD PARTY LIBRARIES -->
+  <!-- BOOTSTRAP CDN -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <!-- FONTAWESOME CDN -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- STANDARD FAVICON TO AVOID CONSOLE ERRORS ON FIREFOX -->
+  <link rel="icon"type="image/x-icon" href="https://boolean.careers/favicon/favicon.ico">
+  <!-- VUE CDN -->
+  <!--<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>-->
+  <!-- LUXON CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/luxon@3.3.0/build/global/luxon.min.js"></script>
+  <!-- AXIOS CDN -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+<body>
+  <div class="container text-center mt-5">
+    <form method="POST" action="filtered.php">
+      <div class="row">
+        <div class="col-6">
+          <select class="form-select" aria-label="Default select example" name="parkingFilter">
+            <option value = "" selected disabled>Filtra per parcheggio</option>
+            <option value="1">Parcheggio</option>
+            <option value="2">Niente parcheggio</option>
+          </select>
+        </div>
+        <div class="col-6">
+          <!--
+          <select class="form-select" aria-label="Default select example">
+            <option selected>Filtra per voto</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="3">4</option>
+            <option value="3">5</option>
+          </select>
+        -->
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-12">
+          <button type="submit" class="btn btn-primary btn-lg" action="filtered.php">Filtra</button>
+        </div>
+      </div>
+    </form>
+    
+    <table class="table mt-5 trable-striped">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Parcheggio</th>
+          <th scope="col">Voto</th>
+          <th scope="col">Distanza dal centro (km)</th>
+        </tr>
+      </thead>
+      <tbody>
+          <?php foreach ($hotels as $singleHotel){
+          ?>
+            <tr>
+              <th scope="row">
+                <?php echo $singleHotel['name']?>
+              </th>
+              <td>
+                <?php echo $singleHotel['parking'] ? "Sì" : "No" ?>
+              </td>
+              <td>
+                <?php echo $singleHotel['vote']?>
+              </td>
+              <td>
+                <?php echo $singleHotel['distance_to_center']?> km
+              </td>
+            </tr>
+          <?php 
+          }
+          ?>
+      </tbody>
+    </table>
+  </div>
+</body>
+<!--<script src="./js/main.js"></script>-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</html>
